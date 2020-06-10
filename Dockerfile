9cb56beee6b8239987fa1faaae9c2aebe0b2d6f2
하성준 <haseongjun@haseongjun-ui-MacBookPro.local>
2020-06-10 17:18:57 +0900

    api test

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,12 @@
 FROM node:12-slim
 
-WORKDIR /starter
+WORKDIR /gimp
 ENV NODE_ENV development
 
-COPY package.json /starter/package.json
+COPY package.json /gimp/package.json
 
 RUN npm install --production
 
-COPY .env.example /starter/.env.example
 COPY . /starter
 
 CMD ["npm","start"]
---
+++ app.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+const { v4: uuidv4 } = require('uuid');
+const axios = require('axios').default;
+
+require('dotenv').config()
+
+const upbit_access_key = process.env.UPBIT_ACCESS_KEY;
+const upbit_secret_key = process.env.UPBIT_SECRET_KEY;
+const upbit_server_url = process.env.UPBIT_SERVER_URL;
+
+
+const payload = {
+  access_key: upbit_access_key,
+  nonce: uuidv4(),
+};
+
+const jwtToken = jwt.sign(payload, upbit_access_key);
+const authorizationToken = `Bearer ${jwtToken}`;
+
+const options = {
+    method: "GET",
+    url: upbit_server_url + "accounts",
+    headers: {Authorization: authorizationToken},
+}
+axios(options)
+.then((response) => {
+    console.log(response.body);
+})
+.catch((err) => {
+    console.log("err!")
+})
